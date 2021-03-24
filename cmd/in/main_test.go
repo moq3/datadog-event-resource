086418276972171c2ad212903971087166099a0b
moq3 <33358876+moq3@users.noreply.github.com>
2021-03-24 18:32:52 -0400

    Update main_test.go

--- cmd/in/main_test.go
+++ cmd/in/main_test.go
@@ -11,7 +11,7 @@ import (
 
 	"bytes"
 
-	"github.com/concourse/datadog-event-resource/cmd"
+	"github.com/moq3/datadog-event-resource/cmd"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gexec"
