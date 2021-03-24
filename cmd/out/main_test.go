d69a1ec4277e1f87a4f74444509f0ad197945093
moq3 <33358876+moq3@users.noreply.github.com>
2021-03-24 18:34:42 -0400

    Update main_test.go

--- cmd/out/main_test.go
+++ cmd/out/main_test.go
@@ -9,7 +9,7 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/concourse/datadog-event-resource/cmd"
+	"github.com/moq3/datadog-event-resource/cmd"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
